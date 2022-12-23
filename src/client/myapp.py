52ec69b55fd39e34137d05cdd7ddcd9826fe21db
Ryan <ryanwallace1396@gmail.com>
2022-12-23 10:43:09 -0800

    client uploads image and renders appropriate prediction displays model details
    Co-authored-by: Margaret Gorlin <margaret.gorlin@gmail.com>
    Co-authored-by: Paul Yim <pauljr97@gmail.com>

--- src/client/myapp.py
+++ src/client/myapp.py
@@ -1,41 +1,73 @@
 import streamlit as st
 import requests
+import os
 
-# title of page
-st.title("Pneumonia Classification")
+print(os.getcwd())
 
-# upload file here
-# initialize file to None
-file = None
-file = st.file_uploader('Upload File With Chest X-Ray')
+def predictionPage():
+  # title of page
+  st.title("Pneumonia Classification")
 
-if file != None:
-  st.image(file, width=500)
+  # upload file here
+  # initialize file to None
+  file = None
+  file = st.file_uploader('Upload File With Chest X-Ray')
 
-# if the predict button is clicked send a request to the server that returns the predicted outcome
-if st.button("Predict"):
   if file != None:
-    print("File: ")
-    print(file)
-    url = "url"
-    payload = "img"
-    print("Predicting!")
-    # response = requests.post(url, json = {'img': payload})
+    st.image(file, width=500)
 
-    # try:
-    #   resp = response.json()
-    #   if resp == 1:
-    #     st.text("Pneumonia Detected")
-    #   elif resp == 0:
-    #     st.text("Pneumonia Not Detected")
-    # except Exception as e:
-    #   st.text(f"Could Not process request because: {e}")
+  # if the predict button is clicked send a request to the server that returns the predicted outcome
+  if st.button("Predict"):
+    if file != None:
+      print(file)
+      url = "http://localhost:5000/predict"
+      payload = "img"
+      print("Predicting!")
+
+      # content-type: 'multipart/form-data'
+      files = {'file': file, 
+      'Content-Type': 'multipart/form-data'}
+      response = requests.post(url, files=files)
+
+      try:
+        resp = response.json()
+        resp = float(resp)
+        if resp >= 0.5:
+          st.header("Pneumonia Detected")
+        elif resp < 0.5:
+          st.header("Pneumonia Not Detected")
+      except Exception as e:
+        st.text(f"Could Not process request because: {e}")
 
 
-st.sidebar.selectbox(
-  'How would you like your prediction results?',
-  ('Email', 'Text')
+def loadModelDetails():
+  st.header('Resnet 50 Model')
+  # st.header('Details:')
+  # st.text('Added one Dense Layer of 256 neurons')
+  # st.text('Dropout set to 0.10')
+  st.markdown(
+    """
+    Model Details:
+    - Resnet50 Base Model 
+    - Added one Dense Layer of 256 neurons
+    - Dropout set to 0.10
+    """
+  )
+  st.image('./images/Classification_Report.png')
+  st.image('./images/Confusion_matrix.png')
+
+section = st.sidebar.selectbox(
+  'What would you like to do?',
+  ('Make Prediction', 'View Model Details')
 )
+
+if section == 'Make Prediction':
+  predictionPage()
+elif section == 'View Model Details':
+  loadModelDetails()
+else: 
+  # Display default 
+  predictionPage()
 
 
 @st.cache
